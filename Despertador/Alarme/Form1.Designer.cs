2cbdad57c49a5fac9d276d70e361b7a652f48b2b
Leandro01832 <leandroleanleo@gmail.com>
2021-08-27 17:23:53 -0300

    despertador

---
+++ Despertador/Alarme/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Alarme
+{
+    partial class FormDesperta
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormDesperta));
+            this.dateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMensagem = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker
+            // 
+            this.dateTimePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePicker.Location = new System.Drawing.Point(198, 61);
+            this.dateTimePicker.Name = "dateTimePicker";
+            this.dateTimePicker.Size = new System.Drawing.Size(151, 34);
+            this.dateTimePicker.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Status:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ligar Despertador";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(207, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(190, 32);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Desligar Despertador";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Horário de alarme:";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon";
+            this.notifyIcon1.Visible = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mensagem:";
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMensagem.Location = new System.Drawing.Point(123, 8);
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(274, 34);
+            this.txtMensagem.TabIndex = 6;
+            // 
+            // FormDesperta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 232);
+            this.Controls.Add(this.txtMensagem);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker);
+            this.Name = "FormDesperta";
+            this.Text = "Despertador";
+            this.Load += new System.EventHandler(this.FormDesperta_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMensagem;
+    }
+}
+
---
+++ Despertador/Alarme/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Alarme
+{
+    public partial class FormDesperta : Form
+    {
+        Timer timer;
+        static string path = Directory.GetCurrentDirectory();
+
+        public FormDesperta()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            label1.Text = "Despertador desligado";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+            label1.Text = "Despertador ligado e rodando...";
+        }
+
+        delegate void UpdateLabel(Label lbl, string value);
+
+        void UpdateDataLabel(Label lbl, string value)
+        {
+            lbl.Text = value;
+        }
+
+        private void FormDesperta_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.Icon = new Icon($@"{path}\favicon.ico");
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Enabled = true;
+            timer.Tick += Timer_Tick;
+               
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+          //  dateTimePicker.Value = dateTimePicker.Value.AddMilliseconds(timer.Interval);
+            DateTime currentTimer = DateTime.Now;
+            DateTime userTimer = dateTimePicker.Value;
+
+            UpdateLabel upd = UpdateDataLabel;
+            if (label1.InvokeRequired)
+                Invoke(upd, label1, "Stop");
+
+            if(currentTimer.Hour == userTimer.Hour &&
+                currentTimer.Minute == userTimer.Minute &&
+                currentTimer.Second == userTimer.Second)
+            {
+                timer.Stop();
+                label1.Text = "Despertador desligado";
+                playSimpleSound();
+               // MessageBox.Show("Ring, Ring, Ring, Ring", "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                notifyIcon1.ShowBalloonTip(2000, "Info", "O seu despertador tocou. " + txtMensagem.Text, ToolTipIcon.Info);
+            }
+        }
+
+        private void playSimpleSound()
+        {
+            SoundPlayer simpleSound = new SoundPlayer($@"{path}\desperta.wav");
+            simpleSound.Play();
+        }
+    }
+}
